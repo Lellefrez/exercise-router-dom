8896956e6706a376e97744dbf77f95ec9c5adab5
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-12-19 18:26:39 +0100

    fixed | sistemataSintassiEScss

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,15 @@
-import './About.scss'
+import "./About.scss";
 
-export default (()=>{
-    return(
-        <>
-         <h1>Chi Siamo?</h1> 
-         <div>
-            <p>
-                Siamo degli studenti del Professor Hyur Scrizzi che ci ha costretto a fare questo sito come esercizio
-            </p>
-         </div>
-        </>
-       
-           )
-})
+export default () => {
+  return (
+    <>
+      <h1>Chi Siamo?</h1>
+      <div className="p-about">
+        <p>
+          Siamo degli studenti del Professor Hyur Scrizzi che ci ha costretto a
+          fare questo sito come esercizio
+        </p>
+      </div>
+    </>
+  );
+};
--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,52 +1,36 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import "./Countries.scss"
-export default (()=>{
-    const [countries, setcountries] = useState([])
-    useEffect(() =>{
-        fetch(`https://restcountries.com/v3.1/all`)
-        .then(response => response.json())
-        .then((obj) => {
-            setcountries(obj);
-            console.log(obj);
-        })
-        .catch(error => console.log(error))
-    }, [])
+import { useEffect } from "react";
+import { useState } from "react";
+import "./Countries.scss";
+import { Link } from "react-router-dom";
+export default () => {
+  const [countries, setcountries] = useState([]);
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/all`)
+      .then((response) => response.json())
+      .then((obj) => {
+        setcountries(obj);
+        console.log(obj);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    
-
-    return(
-        <>
-        <h1>COUNTRIES</h1>
-        <ul className="map-List">
-            {countries.map((country,i)=>{
-                return (
-                   <> 
-                    <div key={`${country.name.common}${i}`}> 
-                    
-                    <div className="Cover-glag-info">
-                        <h2>
-                        {country.name.common}  
-                    </h2>
-                           <figure className="flag">
-                            <img src={countries[i].flags.svg} alt={`Bandiera di ${i}`}/>
-                         </figure>
-                         <div className="Info-Countries">
-                             <p>
-                            Population: {countries[i].population}
-                         </p>
-                         <p>Regione: {countries[i].region}</p>
-                         <p>Capitale:{countries[i].capital}</p>
-                         </div>
-                    </div>
-                      
-                        
-                        </div>
-                   </>
-                 
-                )
-            })}
-        </ul>
-        </>
-    )
-})
+  return (
+    <>
+      <h1>COUNTRIES</h1>
+      <div className="container-flag">
+        {countries.map((country, i) => {
+          return (
+            <Link to={"/"} key={`country${i}`}>
+              <div className="Cover-flag">
+                <h2>{country.name.common}</h2>
+                <figure className="flag">
+                  <img src={countries[i].flags.svg} alt={`Bandiera di ${i}`} />
+                </figure>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+};
